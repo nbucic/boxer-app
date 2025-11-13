7c4c27ac254e43dfcfc6ac95496ac6876ba55b6d
Nikola Bucić <nikola.bucic@gmail.com>
2025-11-13 13:35:31 +0100

    Add AuthContext to prevent going to the index page after refresh

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,52 +1,53 @@
-import { useEffect, useState } from 'react';
-import { Session } from '@supabase/supabase-js';
-import { supabase } from '@/lib/supabase';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
+import { AuthProvider, useAuth } from '@/context/AuthContext';
+import { ActivityIndicator, View } from 'react-native';
 
 const queryClient = new QueryClient();
 
-export default function RootLayout() {
-  const [session, setSession] = useState<Session | null>(null);
+const AppLayout = () => {
+  const { session, isLoading } = useAuth();
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
+  if (isLoading) {
+    return (
+      <View className={'flex-1 justify-center items-center'}>
+        <ActivityIndicator size={'large'} />
+      </View>
+    );
+  }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <GluestackUIProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            animation: 'slide_from_left',
-            statusBarHidden: true,
-            navigationBarHidden: true,
-          }}
-        >
-          <Stack.Protected guard={!!session}>
-            <Stack.Screen name={'(tabs)'} />
-            <Stack.Screen name={'location'} />
-            <Stack.Screen name={'box'} />
-          </Stack.Protected>
+    <GluestackUIProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_left',
+          statusBarHidden: true,
+          navigationBarHidden: true,
+        }}
+      >
+        <Stack.Protected guard={!!session}>
+          <Stack.Screen name={'(tabs)'} />
+          <Stack.Screen name={'location'} />
+          <Stack.Screen name={'box'} />
+        </Stack.Protected>
 
-          <Stack.Protected guard={!session}>
-            <Stack.Screen name={'(auth)/auth'} />
-          </Stack.Protected>
-        </Stack>
-      </GluestackUIProvider>
+        <Stack.Protected guard={!session}>
+          <Stack.Screen name={'(auth)/auth'} />
+        </Stack.Protected>
+      </Stack>
+    </GluestackUIProvider>
+  );
+};
+
+export default function RootLayout() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppLayout />
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
---
+++ context/AuthContext.tsx
@@ -0,0 +1,49 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { Session } from '@supabase/supabase-js';
+import { supabase } from '@/lib/supabase';
+
+type AuthContextType = {
+  session: Session | null;
+  isLoading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  session: null,
+  isLoading: true,
+});
+
+// Custom hook to use the AuthContext
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setIsLoading(false);
+    });
+
+    // Listen for the auth state changes
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, newSession) => {
+      setSession(newSession);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ session, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
