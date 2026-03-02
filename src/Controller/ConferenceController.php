f7cf07c74f5a658184982269755dacacac55c7b1
Nikola Bucić <nikola.bucic@gmail.com>
2026-03-02 14:36:32 +0100

    Listening to Events

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,9 +15,7 @@ final class ConferenceController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
-        ]);
+        return $this->render('conference/index.html.twig');
     }
 
     #[Route('/conference/{id}', name: 'conference')]
